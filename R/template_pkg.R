5b122ceb2ddb2ebb7e508ed4ea7ff8c648946399
Antoine Bichat <a.bichat@yahoo.fr>
2019-08-27 19:13:35 +0200

    Add renv option

--- R/template_pkg.R
+++ R/template_pkg.R
@@ -5,13 +5,15 @@
 #' @param news logical
 #' @param test logical
 #' @param spellcheck logical
+#' @param renv logical
 #'
 #' @return NULL
 #' @export
 #' @importFrom glue glue
 #' @importFrom usethis use_description use_gpl3_license use_readme_rmd
 #' use_build_ignore use_testthat use_spell_check use_news_md
-#' @importFrom rstudioapi navigateToFile
+#' @importFrom rstudioapi navigateToFile restartSession
+#' @importFrom renv init install
 #'
 #' @examples
 #'\dontrun{
@@ -19,7 +21,8 @@
 #'}
 init_package <- function(mail = "antoine.bichat@mines-nancy.org",
                          readme = TRUE, news = TRUE,
-                         test = TRUE, spellcheck = TRUE){
+                         test = TRUE, spellcheck = TRUE,
+                         renv = TRUE){
 
   use_description(fields = list(
     `Authors@R` = glue('person("Antoine", "Bichat", email = "{mail}", \\
@@ -36,7 +39,7 @@ init_package <- function(mail = "antoine.bichat@mines-nancy.org",
 
   if (test) {
     use_testthat()
-    txt_test1 <- "# use_test() \n\n"
+    txt_test1 <- "\n# use_test()"
     txt_test2 <- "\ntest()\n"
   } else {
     txt_test1 <- ""
@@ -51,6 +54,22 @@ init_package <- function(mail = "antoine.bichat@mines-nancy.org",
     txt_spch <- ""
   }
 
+  if(renv) {
+    txt_renv1 <- "\n# renv::install()"
+    txt_renv2 <- glue("\n\n\n#### Package dependencies\n\nrenv",
+                      "::status()\nrenv",
+                      "::snapshot()")
+  } else {
+    txt_renv1 <- ""
+    txt_renv2 <- ""
+  }
+
+  if (test & renv) {
+    txt_sep <- "\n"
+  } else {
+    txt_sep <- ""
+  }
+
   writeLines(text = glue('library(\\
                          devtools)
                          library(\\
@@ -61,10 +80,12 @@ init_package <- function(mail = "antoine.bichat@mines-nancy.org",
                          #                                  \\
                          readme = {readme}, news = {news},
                          #                                  \\
-                         test = {test}, spellcheck = {spellcheck})\n
-                         # use_r() \n
-                         {txt_test1}
-                         #### Repeated\n
+                         test = {test}, spellcheck = {spellcheck},
+                         #                                  \\
+                         renv = {renv})\n
+                         # use_r()
+                         {txt_test1}{txt_sep}{txt_renv1}
+                         \n\n#### Repeated\n
                          load_all()\n
                          document()
                          attachment\\
@@ -75,12 +96,27 @@ init_package <- function(mail = "antoine.bichat@mines-nancy.org",
                          goodpractice\\
                          ::gp()
                          check()
-                         build()
+                         \nbuild()
+                         {txt_renv2}
                          '),
              con = "dev_history.R")
 
   use_build_ignore("dev_history.R")
 
+
+  if(renv) {
+    init(restart = FALSE)
+    install("devtools")
+    install("attachment")
+    install("goodpractice")
+  }
+
   navigateToFile("dev_history.R")
+  Sys.sleep(1)
+
+  if(renv) {
+    restartSession()
+  }
+
 
 }
